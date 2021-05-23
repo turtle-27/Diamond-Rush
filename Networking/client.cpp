fdd61d7889ebd7266035580b9c4e0505790a918f
10mihir <mihirmeena17@gmail.com>
2021-05-23 19:54:48 +0530

    Networking using sockets testing

---
+++ Networking/client.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
+#include <cstring>
+
+#define PORT 1234
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDLNet_Init();
+
+    IPaddress ip;
+    SDLNet_ResolveHost(&ip, "127.0.0.1", PORT);
+    
+    TCPsocket client = SDLNet_TCP_Open(&ip);
+
+    string msg_send;
+    const char* text;
+    char msg_recv[100];
+
+    while(true)
+    {
+        SDLNet_TCP_Recv(client, msg_recv, 100);
+        
+        cout << "Server: " << msg_recv << endl;
+
+        if(strcmp(msg_recv, "quit") == 0)
+        {
+            cout << "Session Terminated." << endl;
+            break;
+        }
+
+        cout << "Client: ";
+        getline(cin, msg_send);
+        text = msg_send.c_str();
+
+        SDLNet_TCP_Send(client, text, strlen(text)+1);
+    }
+    
+    SDLNet_TCP_Close(client);
+    
+    SDLNet_Quit();
+    SDL_Quit;
+}
