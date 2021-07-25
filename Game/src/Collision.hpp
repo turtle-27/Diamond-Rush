6667f931dde2f5e9fac470c00b8ee3d2c67f9f70
10mihir <mihirmeena17@gmail.com>
2021-07-26 00:02:51 +0530

    coins enemies and music added.

--- Game/src/Collision.hpp
+++ Game/src/Collision.hpp
@@ -46,3 +46,35 @@ bool checkCollision( SDL_Rect a, SDL_Rect b )
     //If none of the sides from A are outside B
     return true;
 }
+
+bool touchesWall( SDL_Rect box, std::vector<SDL_Rect> tiles_vc, int TOTAL_TILES)
+{
+    //Go through the tiles
+    for( int i = 0; i < TOTAL_TILES; i++ )
+    {
+        //If the collision box touches the wall tile
+        if( checkCollision( box, tiles_vc[ i ] ) )
+        {
+            return true;
+        }
+    }
+
+    //If no wall tiles were touched
+    return false;
+}
+
+bool touchesCoin( SDL_Rect box, std::vector<SDL_Rect> coins_vc, int TOTAL_COINS)
+{
+    //Go through the coins
+    for( int i = 0; i < TOTAL_COINS; i++ )
+    {
+        //If the collision box touches the wall tile
+        if( checkCollision( box, coins_vc[ i ] ) )
+        {
+            return true;
+        }
+    }
+
+    //If no coins were touched
+    return false;
+}
